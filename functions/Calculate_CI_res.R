80282bb272b789a5846bc66ae85b29aeba6b8fcb
joanl01 <a1825938@adelaide.edu.au>
2024-10-24 20:51:33 +1030

    Function to calculate CI for prop vs unprop

---
+++ functions/Calculate_CI_res.R
@@ -0,0 +1,9 @@
+calculate_CI_res <- function(res, sig_lvl = 0.05){
+  mean_res <- mean(res, na.rm = TRUE)
+  sd_res <- sd(res, na.rm = TRUE)
+  n <- length(res)
+  
+  CI_low <- mean_res - qnorm(1- sig_lvl) * sd_res / sqrt(n)
+  CI_up  <- mean_res + qnorm(1-sig_lvl) * sd_res / sqrt(n)
+  
+}
