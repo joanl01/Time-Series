f081cdf376c5121b8d9eecad8d8e0ef6acc58c2d
joanl01 <a1825938@adelaide.edu.au>
2024-10-28 17:39:05 +1030

    Running code for flagging prop vs unprop

--- analysis-code/04-prop-vs-unprop.R
+++ analysis-code/04-prop-vs-unprop.R
@@ -2,20 +2,24 @@
 pacman::p_load(tidyverse, tseries, forecast, here, readr, skimr)
 fs::dir_ls(here::here("functions")) |> walk(source)
 
+
 # Read in unpropagated data of desired satellites
 sent3a <- read_csv(here::here("satellite_data", "orbital_elements", "unpropagated_elements_Sentinel-3A.csv"))
 sent3b <- read_csv(here::here("satellite_data", "orbital_elements", "unpropagated_elements_Sentinel-3B.csv"))
 sent6a <- read_csv(here::here("satellite_data", "orbital_elements", "unpropagated_elements_Sentinel-6A.csv"))
+
 
 # Read in propagated data of desired satellites
 sent3a_prop <- read_csv(here::here("satellite_data", "orbital_elements", "propagated_elements_Sentinel-3A.csv"))
 sent3b_prop <- read_csv(here::here("satellite_data", "orbital_elements", "propagated_elements_Sentinel-3B.csv"))
 sent6a_prop <- read_csv(here::here("satellite_data", "orbital_elements", "propagated_elements_Sentinel-6A.csv"))
 
+
 # Take the difference between unpropagated and propagated
 sent3a_res <- sent3a[-1,] - sent3a_prop
 sent3b_res <- sent3b[-1,] - sent3b_prop
 sent6a_res <- sent6a[-1,] - sent6a_prop
+
 
 # Apply the function to the residuals
 CI_sent3a_res <- map(sent3a_res, calculate_CI_res)
@@ -23,3 +27,11 @@ CI_sent3b_res <- map(sent3b_res, calculate_CI_res)
 CI_sent6a_res <- map(sent6a_res, calculate_CI_res)
 
 
+# Specify the parameters to check
+parameters <- c("eccentricity", "argument of perigee", "inclination", 
+                "mean anomaly", "Brouwer mean motion", "right ascension")
+
+# Run the function for sent3a_res using CI_sent3a_res
+sent3a_res <- add_flag_columns(sent3a_res, CI_sent3a_res, parameters)
+sent6a_res <- add_flag_columns(sent6a_res, CI_sent6a_res, parameters)
+sent3b_res <- add_flag_columns(sent3b_res, CI_sent3b_res, parameters)
